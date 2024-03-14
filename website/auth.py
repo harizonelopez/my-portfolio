a93bce4f8486a06349f6567c330ebb1b2b2fdefe
Harizone lopez <136503275+harizonelopez@users.noreply.github.com>
2024-03-14 14:05:58 +0300

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -4,12 +4,12 @@ from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login', methods = ['GET', 'POST'])
-def login():
+@auth.route('/about_info', methods = ['GET', 'POST'])
+def about_info():
     if request.method == 'POST':
         return redirect(url_for('views.home'))
         
-    return render_template("login.html")
+    return render_template("info.html")
     
     
 @auth.route('/logout', methods = ['GET', 'POST')
